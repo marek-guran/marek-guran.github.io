ab0952ef9a7e58bca1449838d4b3754b0b21b27f
Marek Guráň <skemgi2@gmail.com>
2023-01-11 12:39:05 +0100

    Service worker now does not cache whole website... Only fonts are now cahed.

--- service-worker.js
+++ service-worker.js
@@ -3,50 +3,13 @@ const CACHE_NAME = 'Marek Guráň';
 
 /* Add relative URL of all the static content you want to store in
 * cache storage (this will help us use our app offline)*/
-let resourcesToCache = ["./",
-  'index.html',
-  '404.html',
-  'projekty.html',
-  'skola.html',
-  'zaujmy.html',
-  'zivotopis.html',
-  'favicon.ico',
-  '/css/404.css',
-  '/css/projekty.css',
-  '/css/skola.css',
-  '/css/style.css',
-  '/css/zaujmy.css',
-  '/css/zivotopis.css',
+let resourcesToCache = [
   '/fonts/josefin_sans/JosefinSans.ttf',
   '/fonts/fontawesome-free-6.2.1-web/css/all.css',
   '/fonts/fontawesome-free-6.2.1-web/webfonts/fa-brands-400.ttf',
   '/fonts/fontawesome-free-6.2.1-web/webfonts/fa-regular-400.ttf',
   '/fonts/fontawesome-free-6.2.1-web/webfonts/fa-solid-900.ttf',
-  '/fonts/fontawesome-free-6.2.1-web/webfonts/fa-v4compatibility.ttf',
-  '/images/android.png',
-  '/images/hardware.png',
-  '/images/hero.jpg',
-  '/images/MG.jpg',
-  '/images/music.png',
-  '/images/gallery/pond.jpg',
-  '/images/gallery/praxmaturita1.JPG',
-  '/images/gallery/praxmaturita2.JPG',
-  '/images/gallery/stuzkova.jpg',
-  '/images/gallery/sunset.jpg',
-  '/images/gallery/waterdrops.jpg',
-  '/images/lineageos_os/logo.png',
-  '/images/lineageos_os/obrazok1.png',
-  '/images/lineageos_os/obrazok2.png',
-  '/images/lineageos_os/obrazok3.png',
-  '/images/marek_guran_app/dark1.png',
-  '/images/marek_guran_app/dark2.png',
-  '/images/marek_guran_app/dark3.png',
-  '/images/marek_guran_app/dark4.png',
-  '/images/marek_guran_app/ikona.png',
-  '/images/rozvrh_app/dark1.png',
-  '/images/rozvrh_app/dark2.png',
-  '/images/rozvrh_app/dark3.png',
-  '/images/rozvrh_app/ikona.png'];
+  '/fonts/fontawesome-free-6.2.1-web/webfonts/fa-v4compatibility.ttf'];
 
 self.addEventListener("install", e => {
   e.waitUntil(
